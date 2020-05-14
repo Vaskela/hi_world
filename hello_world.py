c49c236b3d6022a3914eaadfe944d7e89b2e5e9e
Vaskela Juha <aarnevaskela@gmail.com>
2020-05-14 16:20:26 +0300

    fixed merge

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 print("Hello world!")
-=======
-print("Hello world!")
-print("Hello from feature")
->>>>>>> feature/create-awesome
-=======
 print("Hello once again")
 >>>>>>> merg/feature/add-logging
