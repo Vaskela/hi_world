8ff694a1d5531da49934d689ea17f69d1416543e
Vaskela Juha <aarnevaskela@gmail.com>
2020-05-14 15:38:25 +0300

    merged?

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello world!")
 =======
 print("Hello world!")
 >>>>>>> b23547b242012497282910300f6e6c1d5a27df82
+=======
+print("Hello world!")
+print("Hello from feature")
+>>>>>>> feature/create-awesome
