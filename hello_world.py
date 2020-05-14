73f252d7fef4dcc08c5a571287915de41cb398c9
Vaskela Juha <aarnevaskela@gmail.com>
2020-05-14 16:16:17 +0300

    fix?

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,5 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 print("Hello world!")
-=======
-print("Hello world!")
-<<<<<<< HEAD
->>>>>>> b23547b242012497282910300f6e6c1d5a27df82
 =======
 print("Hello world!")
 print("Hello from feature")
